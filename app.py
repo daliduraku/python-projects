6c272daf8aec2ac697ca8eaf723ed30c876d07ac
daliduraku <mesherbetmolla@gmail.com>
2024-12-01 21:50:41 -0500

    main page of the website is complete mostly working on the other pages

--- app.py
+++ app.py
@@ -10,25 +10,25 @@ def data_types():
     return render_template('data_types.html')
 
 @app.route('/data_structures/')
-def data_types():
+def data_structures():
     return render_template('data_structures.html')
 
 @app.route('/conditionals/')
-def data_types():
+def conditionals():
     return render_template('conditionals.html')
 
 @app.route('/loops/')
-def data_types():
+def loops():
     return render_template('loops.html')
 
 @app.route('/functions/')
-def data_types():
+def functions():
     return render_template('functions.html')
 
 @app.route('/classes/')
-def data_types():
+def classes():
     return render_template('classes.html')
 
 @app.route('/advanced_topics/')
-def data_types():
+def advanced_topics():
     return render_template('advanced_topics.html')
