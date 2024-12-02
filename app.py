8dab72e322d8b127cc6179ad589daa02b08cf1d2
daliduraku <mesherbetmolla@gmail.com>
2024-12-01 20:00:40 -0500

    created the home page and the data types page for the website

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
+@app.route('/data_types/')
+def data_types():
+    return render_template('data_types.html')
